828476a25d4169fc6b703be851080be3fc2b1d39
branmalawi@gmail.com <branmalawi@gmail.com>
2022-07-21 15:25:43 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,7 @@
+const grid = document.querySelectorAll('.grid');
+const styleImg = getComputedStyle(grid[0]);
+const root = document.querySelector(':root');
+
+window.onload = window.onresize = () => {
+  root.style.setProperty('--width',`${styleImg.width}`);
+};
